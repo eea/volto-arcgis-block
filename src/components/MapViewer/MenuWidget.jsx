6756c1703ddd7f83417a25aa32272f6419071f9e
bipoza <bipoza@gmail.com>
2021-11-22 12:42:59 +0100

    eslintrc

--- src/components/MapViewer/MenuWidget.jsx
+++ src/components/MapViewer/MenuWidget.jsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom';
 import React, { createRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { loadModules, loadCss } from 'esri-loader';
-/*eslint import/no-unresolved: 2*/
+// eslint-disable-next-line import/no-unresolved
 import useCartState from '@eeacms/volto-clms-theme/utils/useCartState';
 import { useHistory } from 'react-router-dom';
 import { useIntl } from 'react-intl';
