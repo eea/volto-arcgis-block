3bcf56152d4fff7add74b75aae81dbdc0d2965f4
masanchez85 <masanchez.herrero@bilbomatica.es>
2022-12-21 15:56:40 +0100

    Code style fixes. (ln. 411)

--- src/components/MapViewer/MenuWidget.jsx
+++ src/components/MapViewer/MenuWidget.jsx
@@ -408,7 +408,10 @@ export const TouchScreenPopup = () => {
       <div className="touchScreenPopup-block">
         <div className="touchScreenPopup-content">
           <div className="touchScreenPopup-text">
-            <p>Some functionalities of the map viewer are not available for touchscreens. Functionality will be limited.</p>
+            <p>
+              Some functionalities of the map viewer are not available for touchscreens.
+              Functionality will be limited.
+            </p>
           </div>
         </div>
       </div>
