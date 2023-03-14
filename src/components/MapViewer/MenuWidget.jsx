8c39f17437745cd2840fae73b4883dab5dfe3ff2
ujbolivar <ujbolivar@bilbomatica.es>
2023-03-14 18:13:27 +0100

    CLMS-1634 (FIX): Show Zoom message in Snow and Ice datasets

--- src/components/MapViewer/MenuWidget.jsx
+++ src/components/MapViewer/MenuWidget.jsx
@@ -332,11 +332,12 @@ class MenuWidget extends React.Component {
 
     // add zoomend listener to map to show/hide zoom in message
     this.view.watch('stationary', (isStationary) => {
-      if (isStationary) {
-        let node = document.getElementById('snow-and-ice-zoom-message');
-        if (node) {
+      if (isStationary) {        let nodes = document.querySelectorAll('.zoom-in-message-general');
+        if (nodes) {
           let zoom = this.view.get('zoom');
-          node.style.display = zoom > 6 ? 'none' : 'block';
+          nodes.forEach((node) => {
+            node.style.display = zoom > 6 ? 'none' : 'block';
+          });
         }
       }
     });
@@ -929,7 +930,7 @@ class MenuWidget extends React.Component {
                             <div class="zoom-in-message-container">
                               <span>{product.ProductTitle}</span>
                               <div
-                                class="zoom-in-message"
+                                class="zoom-in-message zoom-in-message-general"
                                 id="snow-and-ice-zoom-message"
                               >
                                 Zoom in to view on map
@@ -1156,17 +1157,42 @@ class MenuWidget extends React.Component {
                   htmlFor={checkIndex}
                   key={'d' + datIndex}
                 >
-                  {description ? (
-                    <Popup
-                      trigger={<span>{dataset.DatasetTitle}</span>}
-                      content={description}
-                      basic
-                      className="custom"
-                      style={{ transform: 'translateX(-5.5rem)' }}
-                    />
-                  ) : (
-                    <span>{dataset.DatasetTitle}</span>
-                  )}
+                  <legend className="ccl-form-legend">
+                    {description ? (
+                      <Popup
+                        trigger={
+                          dataset.ProductId ===
+                            '8474c3b080fa42cc837f1d2338fcf096' ||
+                          dataset.Product === 'Snow and Ice Parameters' ? (
+                            <div class="zoom-in-message-container">
+                              <span>{dataset.DatasetTitle}</span>
+                              <div class="zoom-in-message zoom-in-message-general">
+                                Zoom in to view on map
+                              </div>
+                            </div>
+                          ) : (
+                            <span>{dataset.DatasetTitle}</span>
+                          )
+                        }
+                        content={description}
+                        basic
+                        className="custom"
+                        style={{ transform: 'translateX(-4rem)' }}
+                      />
+                    ) : dataset.ProductId ===
+                        '8474c3b080fa42cc837f1d2338fcf096' ||
+                      dataset.Product ===
+                        'High Resolution Snow and Ice Parameters' ? (
+                      <div class="zoom-in-message-container">
+                        <span>{dataset.DatasetTitle}</span>
+                        <div class="zoom-in-message">
+                          Zoom in to view on map
+                        </div>
+                      </div>
+                    ) : (
+                      <span>{dataset.DatasetTitle}</span>
+                    )}
+                  </legend>
                 </label>
                 <div className="map-menu-icons">
                   {!this.props.download && dataset.IsTimeSeries && (
